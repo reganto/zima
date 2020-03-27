707aed4afa3c471dd39c57b7934281296c8829e3
reganto <rreganto@gmail.com>
2020-03-27 18:49:25 +0430

    add --host and --port to anorc

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import argparse
 from tornado.ioloop import IOLoop
 from tornado.options import parse_command_line
 from anorc import create_app
 
 
+parser = argparse.ArgumentParser(description="Anorc command line")
+parser.add_argument("--host", default="127.0.0.1", type=str, help="Run on the given host.")
+parser.add_argument("--port", default=8000, type=int, help="Run on the given port.")
+args = parser.parse_args()
+
 parse_command_line()
 app = create_app()
-app.listen(8001)
+app.listen(args.port, args.host)
+print("Server started at %s:%s" % (args.host, args.port))
 IOLoop.instance().start()
 
