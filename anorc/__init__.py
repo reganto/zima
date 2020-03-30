feeedbb8adf9da06cc074b71fcd320d22dd05b56
reganto <rreganto@gmail.com>
2020-03-30 18:59:12 +0430

    add a function to convert config class attributes to a dict and assign it to settings

--- anorc/__init__.py
+++ anorc/__init__.py
@@ -5,6 +5,8 @@ from .base import BaseHandler
 
 
 class Route(object):
+    """Anorc router class"""
+
     urls = []
     
     def __call__(self, url, name=None):
@@ -16,13 +18,27 @@ class Route(object):
 route = Route()
 
 
-def create_app():
+def config_class_to_dict(cls):
+    """Convert config class to dict"""
+
+    class_attributes_dict = cls.__dict__
+    result_dict = {}
+    for key, value in class_attributes_dict.items():
+        if not key.startswith("__"):
+            result_dict[key] = value
+
+    return result_dict
+
+
+def create_app(config_name):
+    """App factory"""
+
     from .main import views 
 
 
     class Application(tornado.web.Application):
         def __init__(self):
-            settings = {item[0].lower(): item[1] for item in config.items()} 
+            settings = config_class_to_dict(config.get(config_name))
             super(Application, self).__init__(route.urls, **settings)
 
     app = Application()
