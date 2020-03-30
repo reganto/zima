1ac90289444258063c30180952cc2be089bb2f70
reganto <rreganto@gmail.com>
2020-03-30 19:43:28 +0430

    test_app_in_specific_mode

---
+++ tests/test_app_in_specific_mode.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from anorc import create_app
+
+
+class AppModeTestCase(TestCase):
+    def setUp(self):
+        self.first_app = create_app("testing")
+        self.second_app = create_app("development")
+        self.third_app = create_app("production")
+
+    def test_app_in_testing(self):
+        self.assertTrue(self.first_app.settings["testing"])
+      
+    def test_app_in_developemt(self):
+        self.assertTrue(self.second_app.settings["development"])
+
+    def test_app_in_production(self):
+        self.assertTrue(self.third_app.settings["production"])
+
