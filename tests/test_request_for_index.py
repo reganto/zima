6e8cfbdac6e0db910f30c5db0e3e58531fe941b8
reganto <rreganto@gmail.com>
2020-03-31 15:23:55 +0430

    test_request_for_root_endpoint

---
+++ tests/test_request_for_index.py
@@ -0,0 +1,13 @@
+from tornado.testing import AsyncHTTPTestCase
+from zima import create_app
+
+
+class IndexTestCase(AsyncHTTPTestCase):
+    def get_app(self):
+        return create_app("default")
+
+    def test_homepage(self):
+        response = self.fetch("/")
+        self.assertEqual(response.code, 200)
+        self.assertIn("success", response.body.decode())
+
